d88baa43348f9739b5db9bb817f4b75948d43214
Zhi Chen <instorm2012@gmail.com>
2014-01-07 11:15:04 +0800

    完成了基本的底部菜单

--- MyDailyActivities/src/com/triman/android/app/mydailyactivities/MainActivity.java
+++ MyDailyActivities/src/com/triman/android/app/mydailyactivities/MainActivity.java
@@ -1,65 +1,44 @@
 package com.triman.android.app.mydailyactivities;
 
-import java.util.ArrayList;
-
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
-import android.support.v4.view.ViewPager;
+import android.support.v4.app.FragmentTabHost;
+import android.view.LayoutInflater;
 import android.view.View;
-import android.view.View.OnClickListener;
-import android.widget.Button;
+import android.widget.TabHost.TabSpec;
+import android.widget.TextView;
 
 public class MainActivity extends BaseActivity {
 	
-	private ViewPager vpFragmentContainer;
-	private Button btnActivities;
-	private Button btnDynamics;
-	private Button btnRegistration;
-
+	private FragmentTabHost mTabHost;
+	private LayoutInflater mLayoutInflater;
+	private Class<?> mFragments[] = {ActivitiesFragment.class, DynamicsFragment.class, RegistrationFragment.class};
+	private int mTitles[] = {R.string.activities, R.string.dynamics, R.string.registration};
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		
 		setContentView(R.layout.activity_main);
-		vpFragmentContainer = (ViewPager) findViewById(R.id.vp_fragment_container);
-		setFragmentContainerAdapter(vpFragmentContainer);
 		
-		btnActivities = (Button) findViewById(R.id.btn_activities);
-		btnActivities.setOnClickListener(new MenuClickListener(vpFragmentContainer, 0));
-		
-		btnDynamics = (Button) findViewById(R.id.btn_dynamics);
-		btnDynamics.setOnClickListener(new MenuClickListener(vpFragmentContainer, 1));
-		
-		btnRegistration = (Button) findViewById(R.id.btn_registration);
-		btnRegistration.setOnClickListener(new MenuClickListener(vpFragmentContainer, 2));
+		initView();
 	}
 	
-	private void setFragmentContainerAdapter(ViewPager vpFragmentContainer){
-		ArrayList<Fragment> fragments = new ArrayList<Fragment>();
-		ActivitiesFragment fgActivities = new ActivitiesFragment();
-		DynamicsFragment fgDynamics = new DynamicsFragment();
-		RegistrationFragment fgRegistration = new RegistrationFragment();
-		fragments.add(fgActivities);
-		fragments.add(fgDynamics);
-		fragments.add(fgRegistration);
-		FragmentContainerAdapter fca = new FragmentContainerAdapter(getSupportFragmentManager(), fragments);
-		vpFragmentContainer.setAdapter(fca);
-		vpFragmentContainer.setCurrentItem(0);
+	private void initView(){
+		mLayoutInflater = LayoutInflater.from(this);
+		
+		mTabHost = (FragmentTabHost)findViewById(android.R.id.tabhost);
+        mTabHost.setup(this, getSupportFragmentManager(), R.id.realtabcontent);
+        
+        for(int i = 0; i < mFragments.length; i++){        
+            TabSpec tabSpec = mTabHost.newTabSpec(getResources().getString(mTitles[i])).setIndicator(getTabItemView(i));
+            mTabHost.addTab(tabSpec, mFragments[i], null);
+            mTabHost.getTabWidget().getChildAt(i).setBackgroundResource(R.drawable.menu_background);
+        }
 	}
 	
-	private class MenuClickListener implements OnClickListener{
-		
-		private ViewPager vp;
-		private int index;
-		
-		public MenuClickListener(ViewPager vp, int index){
-			this.vp = vp;
-			this.index = index;
-		}
-
-		@Override
-		public void onClick(View v) {
-			vp.setCurrentItem(index);
-		}
+	private View getTabItemView(int i){
+		View view = mLayoutInflater.inflate(R.layout.tab_item_view, null);
+		TextView textView = (TextView) view.findViewById(R.id.textview);
+		textView.setText(mTitles[i]);
+		return view;
 	}
 }
